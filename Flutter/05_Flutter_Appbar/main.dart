d039d7f08a1fe49445d928420ebf03216e56fc6a
MD. MEHEDI HASAN <127036046+Mehedi-16@users.noreply.github.com>
2025-07-07 07:08:42 +0600

    Update main.dart

--- Flutter/05_Flutter_Appbar/main.dart
+++ Flutter/05_Flutter_Appbar/main.dart
@@ -30,13 +30,27 @@ class HomeActivity extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title:  Text("Home Screen"),
-          titleSpacing: 0,
-          centerTitle: true,
-           toolbarHeight: 60,
-        toolbarOpacity: 1,
-        elevation: 0,
+        title: Text("Home Screen"),
+        centerTitle: true,
         backgroundColor: Colors.cyan,
+        elevation: 4,
+        toolbarHeight: 60,
+        leading: Icon(Icons.menu),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
+        ],
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(20),
+          ),
+        ),
       ),
       body: const Center(child: Text("Welcome to HomeActivity!")),
     );
